3a5271a47e8116e8a9264da86c33161f7d4debde
Eleanor Merry <eleanor.merry@metaswitch.com>
2015-04-08 15:58:25 +0000

    Add stats and alarms

--- src/alarmdefinition.cpp
+++ src/alarmdefinition.cpp
@@ -331,6 +331,22 @@ namespace AlarmDef {
       }
     },
 
+    {CHRONOS_SCALE_IN_PROGRESS, DATABASE_INCONSISTENCY,
+      {
+        {CLEARED,
+          "Chronos: Chronos timers are synchronized",
+          "The local Chronos process is synchronized with the rest of the cluster. If a scale-up or "
+          "scale-down operation is in progress, it can now be completed."
+        },
+        {MINOR,
+          "Chronos: Chrono database resynchronization in progress",
+          "The local Chronos process is resynchronizing with the rest of the cluster. Service should "
+          "be unaffected. If a scale-up or scale-down operation is in progress, wait for resynchronization to "
+          "finish before completing the operation."
+        }
+      }
+    },
+
     {MEMCACHED_PROCESS_FAIL, SOFTWARE_ERROR,
       {
         {CLEARED,
