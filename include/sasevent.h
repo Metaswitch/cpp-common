1ef3f8e5b54db048c42e4b37ce3f94475a634183
Tom Rees <Thomas.Rees@metaswitch.com>
2015-06-24 17:37:49 +0100

    Add SAS logs and update datestamp
    Conflicts:
    
    	include/sasevent.h

--- include/sasevent.h
+++ include/sasevent.h
@@ -42,7 +42,7 @@
 
 namespace SASEvent {
 
-  const std::string CURRENT_RESOURCE_BUNDLE_DATESTAMP = "20150519";
+  const std::string CURRENT_RESOURCE_BUNDLE_DATESTAMP = "20150624";
   const std::string RESOURCE_BUNDLE_NAME = "org.projectclearwater";
   const std::string CURRENT_RESOURCE_BUNDLE =
                  RESOURCE_BUNDLE_NAME + "." + CURRENT_RESOURCE_BUNDLE_DATESTAMP;
