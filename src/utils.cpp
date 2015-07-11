a207afdbba7cc810a0f0d8917392cc07e742367d
Graeme Robertson <graeme.robertson@metaswitch.com>
2015-07-11 14:34:05 +0000

    Separate xml escaping into two functions

--- src/utils.cpp
+++ src/utils.cpp
@@ -157,31 +157,24 @@ std::string Utils::url_escape(const std::string& s)
 
 std::string Utils::xml_escape(const std::string& s)
 {
-  if (s.find_first_of("&\"'<>") != std::string::npos)
-  {
-    std::string r;
-    r.reserve(2*s.length());  // Reserve enough space to avoid continually reallocating.
+  std::string r;
+  r.reserve(2*s.length());  // Reserve enough space to avoid continually reallocating.
 
-    for (size_t ii = 0; ii < s.length(); ++ii)
+  for (size_t ii = 0; ii < s.length(); ++ii)
+  {
+    switch (s[ii])
     {
-      switch (s[ii])
-      {
-      case '&':  r.append("&amp;"); break;
-      case '\"': r.append("&quot;"); break;
-      case '\'': r.append("&apos;"); break;
-      case '<':  r.append("&lt;"); break;
-      case '>':  r.append("&gt;"); break;
+    case '&':  r.append("&amp;"); break;
+    case '\"': r.append("&quot;"); break;
+    case '\'': r.append("&apos;"); break;
+    case '<':  r.append("&lt;"); break;
+    case '>':  r.append("&gt;"); break;
 
-      // Otherwise, append the literal character
-      default: r.push_back(s[ii]); break;
-      }
+               // Otherwise, append the literal character
+    default: r.push_back(s[ii]); break;
     }
-    return r;
   }
-  else
-  {
-    return s;
-  }
+  return r;
 }
 
 
