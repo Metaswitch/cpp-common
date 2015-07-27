83bc043393a75d55bb6eed2b3449994e1e6c5026
Tom Rees <Thomas.Rees@metaswitch.com>
2015-07-27 14:56:58 +0100

    Update SAS bundle ID

--- include/sasevent.h
+++ include/sasevent.h
@@ -42,7 +42,7 @@
 
 namespace SASEvent {
 
-  const std::string CURRENT_RESOURCE_BUNDLE_DATESTAMP = "20150728";
+  const std::string CURRENT_RESOURCE_BUNDLE_DATESTAMP = "20150729";
   const std::string RESOURCE_BUNDLE_NAME = "org.projectclearwater";
   const std::string CURRENT_RESOURCE_BUNDLE =
                  RESOURCE_BUNDLE_NAME + "." + CURRENT_RESOURCE_BUNDLE_DATESTAMP;
