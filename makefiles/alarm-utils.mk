cae2fb889e4a3e31f6708fb786cd1151f8ef70f8
Andy Caldwell <andrew.caldwell@metaswitch.com>
2015-12-07 03:05:08 +0000

    More flexibility for alarm header generation

--- makefiles/alarm-utils.mk
+++ makefiles/alarm-utils.mk
@@ -1,7 +1,7 @@
 # Utility function for the alarm header creation. The list of files here
 # should be kept in sync with the main alarm-header makefile
-${BUILD_DIR}/bin/alarm_header : ../modules/cpp-common/src/json_alarms.cpp \
-                                ../modules/cpp-common/src/alarm_header.cpp \
-                                ../modules/cpp-common/src/alarmdefinition.cpp \
-                                ../modules/cpp-common/makefiles/alarms-header.mk
-	ROOT=${ROOT} ${MAKE} -f ../modules/cpp-common/makefiles/alarms-header.mk
+${BUILD_DIR}/bin/alarm_header : ${MODULE_DIR}/cpp-common/src/json_alarms.cpp \
+                                ${MODULE_DIR}/cpp-common/src/alarm_header.cpp \
+                                ${MODULE_DIR}/cpp-common/src/alarmdefinition.cpp \
+                                ${MODULE_DIR}/cpp-common/makefiles/alarms-header.mk
+	ROOT=${ROOT} MODULE_DIR=${MODULE_DIR} BUILD_DIR=${BUILD_DIR} ${MAKE} -f ${MODULE_DIR}/cpp-common/makefiles/alarms-header.mk
--- makefiles/alarms-header.mk
+++ makefiles/alarms-header.mk
@@ -4,10 +4,10 @@ alarm_header_SOURCES := alarm_header.cpp \
                         json_alarms.cpp \
                         alarmdefinition.cpp
 
-alarm_header_CPPFLAGS := -I../modules/cpp-common/include \
-                         -I../modules/rapidjson/include
+alarm_header_CPPFLAGS := -I${MODULE_DIR}/cpp-common/include \
+                         -I${MODULE_DIR}/rapidjson/include
 
 # Add cpp-common/src as VPATH so build will find modules there.
-VPATH = ../modules/cpp-common/src
+VPATH = ${MODULE_DIR}/cpp-common/src
 
 include ${ROOT}/build-infra/cpp.mk
