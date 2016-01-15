2323ca1ddfa77c330123838b9706aa22a7d585b5
Adam Lindley <adam.lindley@metaswitch.com>
2016-01-15 17:16:05 +0000

    update communication monitor to handle partial error log

--- src/communicationmonitor.cpp
+++ src/communicationmonitor.cpp
@@ -49,9 +49,12 @@ CommunicationMonitor::CommunicationMonitor(Alarm* alarm,
   _receiver(receiver),
   _clear_confirm_ms(clear_confirm_sec * 1000),
   _set_confirm_ms(set_confirm_sec * 1000),
-  _error_state(false)
+  _previous_state(0),
+  _new_state(0)
 {
   _next_check = current_time_ms() + _set_confirm_ms;
+  // Setup the possible error states
+  enum { NO_ERRORS, SOME_ERRORS, ONLY_ERRORS };
 }
 
 CommunicationMonitor::~CommunicationMonitor()
@@ -81,37 +84,104 @@ void CommunicationMonitor::track_communication_changes(unsigned long now_ms)
       TRC_DEBUG("Checking communication changes - successful attempts %d, failures %d",
                 succeeded, failed);
 
-      // Check if we need to raise any logs/alarms. We do so if:
-      //  - We're not currently errored, and we've seen no successes and
-      //    at least one error in the last 'clear_confirm' ms
-      //  - We're currently errored, and we've seen at least one success
-      //    in the last 'set_confirm' ms.
-      if ((!_error_state) && (succeeded == 0) && (failed != 0))
+      // Determine the current error state based on the results.
+      // If no successes of failures, new state remains unchanged.
+      // States:
+      // NO_ERRORS: At least one success and no failures
+      // SOME_ERRORS: At least one success and at least one failure
+      // ONLY_ERRORS: No successes and at least one failure
+      if ((succeeded != 0) && (failed == 0))
       {
-        _error_state = true;
-        CL_CM_CONNECTION_ERRORED.log(_sender.c_str(),
-                                     _receiver.c_str());
-
-        if ((_alarm != NULL) && (!_alarm->alarmed()))
-        {
-          TRC_STATUS("Setting alarm %d", _alarm->index());
-          _alarm->set();
-        }
+        _new_state = NO_ERRORS;
       }
-      else if ((_error_state) && (succeeded != 0))
+      else if ((succeeded != 0) && (failed != 0))
       {
-        _error_state = false;
-        CL_CM_CONNECTION_CLEARED.log(_sender.c_str(),
-                                     _receiver.c_str());
-
-        if ((_alarm != NULL) && (_alarm->alarmed()))
-        {
-          TRC_STATUS("Clearing alarm %d", _alarm->index());
-          _alarm->clear();
-        }
+        _new_state = SOME_ERRORS;
+      }
+      else if ((succeeded == 0) && (failed != 0))
+      {
+        _new_state = ONLY_ERRORS;
       }
 
-      _next_check = _error_state ? now_ms + _clear_confirm_ms :
+      // Check if we need to raise any logs/alarms. We do so if the new
+      // error state is different from the the previous error state.
+      //
+      // Switch organised by previous state, then new state.
+      switch (_previous_state)
+      {
+        case NO_ERRORS:
+          switch (_new_state)
+          {
+            case NO_ERRORS:
+              // No change in state. Do nothing.
+              break;
+
+            case SOME_ERRORS:
+              // Raise the partial error log
+              CL_CM_CONNECTION__PARTIAL_ERROR.log(_sender.c_str(),
+                                                  _receiver.c_str());
+              break;
+
+            case ONLY_ERRORS:
+              // Raise the full error log, and set alarm
+              CL_CM_CONNECTION_ERRORED.log(_sender.c_str(),
+                                           _receiver.c_str());
+              TRC_STATUS("Setting alarm %d", _alarm->index());
+              _alarm->set();
+              break;
+          }
+        case SOME_ERRORS:
+          switch (_new_state)
+          {
+            case NO_ERRORS:
+              // Clear the partial error log
+              CL_CM_CONNECTION_CLEARED.log(_sender.c_str(),
+                                           _receiver.c_str());
+              break;
+
+            case SOME_ERRORS:
+              // No change in state. Do nothing.
+              break;
+
+            case ONLY_ERRORS:
+              // Raise the full error log, and set the alarm.
+              CL_CM_CONNECTION_ERRORED.log(_sender.c_str(),
+                                           _receiver.c_str());
+              TRC_STATUS("Setting alarm %d", _alarm->index());
+              _alarm->set();
+              break;
+          }
+        case ONLY_ERRORS:
+          switch (_new_state)
+          {
+            case NO_ERRORS:
+              // Clear the full error log, clear alarm.
+              CL_CM_CONNECTION_CLEARED.log(_sender.c_str(),
+                                           _receiver.c_str());
+              TRC_STATUS("Clearing alarm %d", _alarm->index());
+              _alarm->clear();
+              break;
+
+            case SOME_ERRORS:
+              // Clear the alarm, raise the partial error log.
+              CL_CM_CONNECTION_CLEARED.log(_sender.c_str(),
+                                           _receiver.c_str());
+              TRC_STATUS("Clearing alarm %d", _alarm->index());
+              _alarm->clear();
+              break;
+
+            case ONLY_ERRORS:
+              // No change in state. Do nothing.
+              break;
+          }
+      }
+
+      // Set the previous state to the new state, as operation is finished.
+      _previous_state = _new_state;
+
+      // Set the next check interval.
+      // Evaluates TRUE if either SOME_ERRORS or ONLY_ERRORS.
+      _next_check = _new_state ? now_ms + _clear_confirm_ms :
                                    now_ms + _set_confirm_ms;
     }
 
