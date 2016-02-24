664ce64089bf8c7a31866d99c23f59672376ed71
Rob Day <Robert.Day@metaswitch.com>
2016-02-24 20:52:08 +0000

    Update SAS resource bundle datestamp

--- include/sasevent.h
+++ include/sasevent.h
@@ -42,7 +42,7 @@
 
 namespace SASEvent {
 
-  const std::string CURRENT_RESOURCE_BUNDLE_DATESTAMP = "20160119";
+  const std::string CURRENT_RESOURCE_BUNDLE_DATESTAMP = "20160224";
   const std::string RESOURCE_BUNDLE_NAME = "org.projectclearwater";
   const std::string CURRENT_RESOURCE_BUNDLE =
                  RESOURCE_BUNDLE_NAME + "." + CURRENT_RESOURCE_BUNDLE_DATESTAMP;
