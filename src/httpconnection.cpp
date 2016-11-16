f2f2ae2bb98204e78562f5fffa61a92ce052cbdd
David Hotham <david.hotham@metaswitch.com>
2016-11-16 17:25:14 +0000

    https support tidying
    this version is closer to the one that I submitted upstream

--- src/httpconnection.cpp
+++ src/httpconnection.cpp
@@ -599,56 +599,17 @@ HTTPCode HttpConnection::send_request(const std::string& path,                 /
     TRC_DEBUG("recycle_conn: %d", recycle_conn);
     curl_easy_setopt(curl, CURLOPT_FRESH_CONNECT, recycle_conn ? 1L : 0L);
 
-    //@@@JPB - Changes to support https, where we must pass the domain name in the url
-    // rather than a raw IP address.
-    std::string ip_url;
-    struct curl_slist *host_resolve_add_addr = NULL;
-    struct curl_slist *host_resolve_remove_addr = NULL;
-
     char buf[100];
     remote_ip = inet_ntop(i->address.af, &i->address.addr, buf, sizeof(buf));
 
-    if (_scheme == "http")
-    {
-      TRC_DEBUG("scheme == http");
+    std::string resolve_addr =
+      _host + ":" + std::to_string(i->port) + ":" + remote_ip;
+    struct curl_slist *host_resolve_add_addr = curl_slist_append(NULL, resolve_addr.c_str());
 
-      // Convert the target IP address into a string and fix up the URL.  It
-      // would be nice to use curl_easy_setopt(CURL_RESOLVE) here, but its
-      // implementation is incomplete.
-      if (i->address.af == AF_INET6)
-      {
-        ip_url = "http://[" + std::string(remote_ip) + "]:" + std::to_string(i->port) + path;
-      }
-      else
-      {
-        ip_url = "http://" + std::string(remote_ip) + ":" + std::to_string(i->port) + path;
-      }
-    }
-    else
-    {
-      TRC_DEBUG("scheme == https");
+    TRC_DEBUG("Set CURLOPT_RESOLVE: %s", resolve_addr.c_str());
+    curl_easy_setopt(curl, CURLOPT_RESOLVE, host_resolve_add_addr);
 
-      // https
-      ip_url = "https://" + _host + ":" + std::to_string(i->port) + path;
-      TRC_DEBUG("ip_url: %s", ip_url.c_str());
-
-      std::string resolve_addr =
-        _host + ":" + std::to_string(i->port) + ":" + remote_ip;
-      TRC_DEBUG("resolve_addr: %s", resolve_addr.c_str());
-
-      host_resolve_add_addr = curl_slist_append(
-        host_resolve_add_addr,
-        resolve_addr.c_str());
-      TRC_DEBUG("Created host_resolve_add_addr");
-
-      host_resolve_remove_addr = curl_slist_append(
-        host_resolve_remove_addr,
-        (std::string("-") + _host + ":" + std::to_string(i->port)).c_str());
-      TRC_DEBUG("Created host_resolve_remove_addr");
-
-      TRC_DEBUG("Set CURLOPT_RESOLVE: %s", resolve_addr.c_str());
-      curl_easy_setopt(curl, CURLOPT_RESOLVE, host_resolve_add_addr);
-    }
+    std::string ip_url = _scheme + "://" + _host + ":" + std::to_string(i->port) + path;
     TRC_DEBUG("Set CURLOPT_URL: %s", ip_url.c_str());
     curl_easy_setopt(curl, CURLOPT_URL, ip_url.c_str());
 
@@ -667,23 +628,18 @@ HTTPCode HttpConnection::send_request(const std::string& path,                 /
     TRC_DEBUG("Sending HTTP request : %s (trying %s) %s", url.c_str(), remote_ip, (recycle_conn) ? "on new connection" : "");
     rc = curl_easy_perform(curl);
 
+    curl_slist_free_all(host_resolve_add_addr);
+    std::string remove_addr = std::string("-") + _host + ":" + std::to_string(i->port);
+    struct curl_slist *host_resolve_remove_addr = curl_slist_append(
+      NULL,
+      remove_addr.c_str());
+    curl_easy_setopt(curl, CURLOPT_RESOLVE, host_resolve_remove_addr);
+    curl_slist_free_all(host_resolve_remove_addr);
+
     // If a request was sent, log it to SAS.
     if (recorder.request.length() > 0)
     {
       sas_log_http_req(trail, curl, method_str, url, recorder.request, req_timestamp, 0);
-    }
-
-    if (host_resolve_add_addr)
-    {
-      curl_slist_free_all(host_resolve_add_addr);
-      host_resolve_add_addr = NULL;
-    }
-
-    if (host_resolve_remove_addr)
-    {
-      curl_easy_setopt(curl, CURLOPT_RESOLVE, host_resolve_remove_addr);
-      curl_slist_free_all(host_resolve_remove_addr);
-      host_resolve_remove_addr = NULL;
     }
 
     // Log the result of the request.
