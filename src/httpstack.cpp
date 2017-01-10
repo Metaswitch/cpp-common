0c4eca6516026f6854cb07f2dd0ede8d6e187215
Oyvind Almelid <oyvind.almelid@metaswitch.com>
2017-01-10 10:12:22 +0000

    Made the error cases in get_x_real_ip_port clearer

--- src/httpstack.cpp
+++ src/httpstack.cpp
@@ -479,6 +479,8 @@ bool HttpStack::Request::get_x_real_ip_port(std::string& ip, unsigned short& por
     ip = real_ip;
     std::string port_s = header("X-Real-Port");
     int port_i;
+    // We have a real IP, if we cannot get a real port we fail softly
+    port = 0;
 
     if (port_s != "" && (std::all_of(port_s.begin(), port_s.end(), ::isdigit)))
     {
@@ -488,15 +490,11 @@ bool HttpStack::Request::get_x_real_ip_port(std::string& ip, unsigned short& por
       }
       catch (...)
       {
-        port_i = 0;
+        port_i = -1;
       }
-      if (port_i > 0 && port_i < USHRT_MAX)
+      if (port_i >= 0 && port_i < USHRT_MAX)
       {
         port = (short)port_i;
-      }
-      else
-      {
-        port = 0;
       }
     }
   }
