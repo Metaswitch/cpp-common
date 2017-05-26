f881e9cd7c614b23b40e5306408e71cbf943a209
Mat Meredith <matthew.meredith@metaswitch.com>
2017-05-26 13:13:13 +0000

    Add missing copyright headers

--- src/unique.cpp
+++ src/unique.cpp
@@ -1,3 +1,13 @@
+/**
+ * Copyright (C) Metaswitch Networks 2017
+ * If license terms are provided to you in a COPYING file in the root
+ * directory
+ * of the source code repository by which you are accessing this code, then
+ * the license outlined in that COPYING file applies to your use.
+ * Otherwise no rights are granted except for those provided to you by
+ * Metaswitch Networks in a separate written agreement.
+ */
+
 #include "utils.h"
 
 #include <atomic>
@@ -23,7 +33,7 @@ uint64_t generate_unique_integer(uint32_t deployment_id, uint32_t instance_id)
   uint64_t timestamp = ((uint64_t)ts.tv_sec * 1000) + (ts.tv_nsec / 1000000);
   uint32_t local_sequence_number = sequence_number++;
 
-  uint64_t rc = (timestamp << timestamp_shift) | 
+  uint64_t rc = (timestamp << timestamp_shift) |
                 (deployment_id << deployment_id_shift) |
                 (instance_id << instance_id_shift) |
                 (local_sequence_number & sequence_mask);
