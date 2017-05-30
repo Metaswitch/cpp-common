ad85c463c7c5abe78ecd5dd6f43e6a763976c295
Mat Meredith <matthew.meredith@metaswitch.com>
2017-05-30 10:02:26 +0000

    Fix Copyright dates

--- include/snmp_scalar_by_scope_table.h
+++ include/snmp_scalar_by_scope_table.h
@@ -1,7 +1,7 @@
 /**
  * @file snmp_scalar_by_scope__table.h
  *
- * Copyright (C) Metaswitch Networks 2017
+ * Copyright (C) Metaswitch Networks 2016
  * If license terms are provided to you in a COPYING file in the root directory
  * of the source code repository by which you are accessing this code, then
  * the license outlined in that COPYING file applies to your use.
--- test_utils/mock_cassandra_connection_pool.h
+++ test_utils/mock_cassandra_connection_pool.h
@@ -2,7 +2,7 @@
  * @file mock_cassandra_connection_pool.h Mock CassandraConnectionPool for
  * testing.
  *
- * Copyright (C) Metaswitch Networks 2017
+ * Copyright (C) Metaswitch Networks 2016
  * If license terms are provided to you in a COPYING file in the root directory
  * of the source code repository by which you are accessing this code, then
  * the license outlined in that COPYING file applies to your use.
