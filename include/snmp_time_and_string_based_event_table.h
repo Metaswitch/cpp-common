8fc1c85868efa62c6871d8615b1158d24284e962
Mat Meredith <matthew.meredith@metaswitch.com>
2017-07-24 16:41:04 +0000

    Fix copyright statement

--- include/snmp_time_and_string_based_event_table.h
+++ include/snmp_time_and_string_based_event_table.h
@@ -1,7 +1,7 @@
 /**
  * @file snmp_time_and_string_based_event_table.h
  *
- * Copyright (C) Metaswitch Networks 2015
+ * Copyright (C) Metaswitch Networks 2017
  * If license terms are provided to you in a COPYING file in the root directory
  * of the source code repository by which you are accessing this code, then
  * the license outlined in that COPYING file applies to your use.
