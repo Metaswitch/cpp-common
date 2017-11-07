a099f854b52080740709b24a34378a07dc82b578
Mat Meredith <matthew.meredith@metaswitch.com>
2017-11-07 14:58:32 +0000

    Go for lower timeout

--- src/memcachedstore.cpp
+++ src/memcachedstore.cpp
@@ -46,7 +46,7 @@ BaseMemcachedStore::BaseMemcachedStore(bool binary,
   // timeout because libmemcached tries to connect to all servers sequentially
   // during start-up, and if any are not up we don't want to wait for any
   // significant length of time.
-  _options = "--CONNECT-TIMEOUT=10 --SUPPORT-CAS --POLL-TIMEOUT=500";
+  _options = "--CONNECT-TIMEOUT=10 --SUPPORT-CAS --POLL-TIMEOUT=100";
   _options += (_binary) ? " --BINARY-PROTOCOL" : "";
 }
 
