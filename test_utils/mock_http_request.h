c21b341dfaa74a4ac1cd09f2e5a755ba948c6fbc
Adam Lindley <adam.lindley@metaswitch.com>
2018-01-17 11:01:18 +0000

    Adds mock HttpRequest class

---
+++ test_utils/mock_http_request.h
@@ -0,0 +1,37 @@
+/**
+ * @file mock_http_request.h
+ *
+ * Copyright (C) Metaswitch Networks 2018
+ * If license terms are provided to you in a COPYING file in the root directory
+ * of the source code repository by which you are accessing this code, then
+ * the license outlined in that COPYING file applies to your use.
+ * Otherwise no rights are granted except for those provided to you by
+ * Metaswitch Networks in a separate written agreement.
+ */
+
+#ifndef MOCK_HTTP_REQUEST_H__
+#define MOCK_HTTP_REQUEST_H__
+
+#include "http_request.h"
+
+class MockHttpRequest : public HttpRequest
+{
+public:
+  MockHttpRequest();
+  ~MockHttpRequest();
+
+  // SET methods
+  MOCK_METHOD2(set_req_body, void(std::string body);
+  MOCK_METHOD2(set_req_headers, void(std::string req_header);
+  MOCK_METHOD2(set_sas_trail, void(SAS::TrailId trail);
+  MOCK_METHOD2(set_allowed_host_state, void(int allowed_host_state);
+  MOCK_METHOD2(set_username, void(std::string username);
+
+  // Sends the request and populates ret code, recv headers, and recv body
+  // Takes a RequestType, as defined in httpclient.h
+  MOCK_METHOD2(send, HttpResponse(HttpClient::RequestType request_type);
+};
+
+#endif
+
+
