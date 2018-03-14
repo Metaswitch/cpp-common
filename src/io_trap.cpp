5b2600fa13be432ef05d30a161d0045dd64978fd
Alex Hockey <alex.hockey@metaswitch.com>
2018-03-14 09:42:13 +0000

    Make network operation checking more sophisticated.
    - Intercept poll, epoll, and select, and fail if any of these functions are called incorrectly.
    - Only fail on per-socket functions (connect, send, recv, etc.) if the socket is blocking.

--- src/io_trap.cpp
+++ src/io_trap.cpp
@@ -14,6 +14,10 @@ extern "C" {
 #include <sys/socket.h>
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/select.h>
+#include <poll.h>
 }
 
 #include <atomic>
@@ -29,7 +33,7 @@ static void maybe_abort()
   }
 }
 
-#define INTERPOSE(FUNCTION, ...)                                               \
+#define HANDLE_NON_FD_CALL(FUNCTION, ...)                                      \
   using FunctionType = decltype(&FUNCTION);                                    \
   static std::atomic<FunctionType> real_function(nullptr);                     \
   if (!real_function)                                                          \
@@ -41,50 +45,101 @@ static void maybe_abort()
                                                                                \
   return real_function(__VA_ARGS__)
 
+#define HANDLE_FD_CALL(FUNCTION, FD, ...)                                      \
+  using FunctionType = decltype(&FUNCTION);                                    \
+  static std::atomic<FunctionType> real_function(nullptr);                     \
+  if (!real_function)                                                          \
+  {                                                                            \
+    real_function = (FunctionType)dlsym(RTLD_NEXT, #FUNCTION);                 \
+  }                                                                            \
+                                                                               \
+  if ((fcntl((FD), F_GETFL) & O_NONBLOCK) == 0)                                \
+  {                                                                            \
+    maybe_abort();                                                             \
+  }                                                                            \
+                                                                               \
+  return real_function(FD, __VA_ARGS__)
 
 extern "C" ssize_t recv(int sockfd, void *buf, size_t len, int flags)
 {
-  INTERPOSE(recv, sockfd, buf, len, flags);
+  HANDLE_FD_CALL(recv, sockfd, buf, len, flags);
 }
 
 extern "C" ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
                             struct sockaddr *dest_addr, socklen_t* addrlen)
 {
-  INTERPOSE(recvfrom, sockfd, buf, len, flags, dest_addr, addrlen);
+  HANDLE_FD_CALL(recvfrom, sockfd, buf, len, flags, dest_addr, addrlen);
 }
 
 extern "C" ssize_t recvmsg(int sockfd, struct msghdr *msg, int flags)
 {
-  INTERPOSE(recvmsg, sockfd, msg, flags);
+  HANDLE_FD_CALL(recvmsg, sockfd, msg, flags);
 }
 
 extern "C" ssize_t send(int sockfd, const void *buf, size_t len, int flags)
 {
-  INTERPOSE(send, sockfd, buf, len, flags);
+  HANDLE_FD_CALL(send, sockfd, buf, len, flags);
 }
 
 extern "C" ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
                           const struct sockaddr *dest_addr, socklen_t addrlen)
 {
-  INTERPOSE(sendto, sockfd, buf, len, flags, dest_addr, addrlen);
+  HANDLE_FD_CALL(sendto, sockfd, buf, len, flags, dest_addr, addrlen);
 }
 
 extern "C" ssize_t sendmsg(int sockfd, const struct msghdr *msg, int flags)
 {
-  INTERPOSE(sendmsg, sockfd, msg, flags);
+  HANDLE_FD_CALL(sendmsg, sockfd, msg, flags);
 }
 
 extern "C" int connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen)
 {
-  INTERPOSE(connect, sockfd, addr, addrlen);
+  HANDLE_FD_CALL(connect, sockfd, addr, addrlen);
 }
 
 extern "C" int accept(int sockfd, struct sockaddr *addr, socklen_t *addrlen)
 {
-  INTERPOSE(accept, sockfd, addr, addrlen);
+  HANDLE_FD_CALL(accept, sockfd, addr, addrlen);
 }
 
 extern "C" int accept4(int sockfd, struct sockaddr *addr, socklen_t *addrlen, int flags)
 {
-  INTERPOSE(accept4, sockfd, addr, addrlen, flags);
+  HANDLE_FD_CALL(accept4, sockfd, addr, addrlen, flags);
+}
+
+extern "C" int select(int nfds, fd_set *readfds, fd_set *writefds,
+                      fd_set *exceptfds, struct timeval *timeout)
+{
+  HANDLE_NON_FD_CALL(select, nfds, readfds, writefds, exceptfds, timeout);
+}
+
+extern "C" int pselect(int nfds, fd_set *readfds, fd_set *writefds,
+                       fd_set *exceptfds, const struct timespec *timeout,
+                       const sigset_t *sigmask)
+{
+  HANDLE_NON_FD_CALL(pselect, nfds, readfds, writefds, exceptfds, timeout, sigmask);
+}
+
+extern "C" int epoll_wait(int epfd, struct epoll_event *events,
+                          int maxevents, int timeout)
+{
+  HANDLE_NON_FD_CALL(epoll_wait, epfd, events, maxevents, timeout);
+}
+
+extern "C" int epoll_pwait(int epfd, struct epoll_event *events,
+                           int maxevents, int timeout,
+                           const sigset_t *sigmask)
+{
+  HANDLE_NON_FD_CALL(epoll_pwait, epfd, events, maxevents, timeout, sigmask);
+}
+
+extern "C" int poll(struct pollfd *fds, nfds_t nfds, int timeout)
+{
+  HANDLE_NON_FD_CALL(poll, fds, nfds, timeout);
+}
+
+extern "C" int ppoll(struct pollfd *fds, nfds_t nfds,
+                     const struct timespec *tmo_p, const sigset_t *sigmask)
+{
+  HANDLE_NON_FD_CALL(ppoll, fds, nfds, tmo_p, sigmask);
 }
